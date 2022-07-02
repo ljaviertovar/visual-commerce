d05ec76c08a873ae14d2eaecb25a29d30d94424c
ljaviertovar <luisjavier_tovar@outlook.com>
2022-07-02 17:24:27 -0500

    feat: add menu

--- components/ui/NavbarStore.tsx
+++ components/ui/NavbarStore.tsx
@@ -1,11 +1,30 @@
+import {
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuItemOption,
+	MenuGroup,
+	MenuOptionGroup,
+	MenuDivider,
+	Button,
+} from "@chakra-ui/react"
+import { ChevronDownIcon } from "@chakra-ui/icons"
+
 const NavbarStore = () => {
 	return (
-		<nav>
-			<ul>
-				<li>Para Hombre</li>
-				<li>Para Mujer</li>
-			</ul>
-		</nav>
+		<Menu>
+			<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+				Actions
+			</MenuButton>
+			<MenuList>
+				<MenuItem>Download</MenuItem>
+				<MenuItem>Create a Copy</MenuItem>
+				<MenuItem>Mark as Draft</MenuItem>
+				<MenuItem>Delete</MenuItem>
+				<MenuItem>Attend a Workshop</MenuItem>
+			</MenuList>
+		</Menu>
 	)
 }
 
