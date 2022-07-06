f1e2617c3c7e1747225b450ae48873ce4fb38e9f
ljaviertovar <luisjavier_tovar@outlook.com>
2022-07-06 08:34:33 -0500

    feat: working on Header

--- components/ui/HeaderStore.tsx
+++ components/ui/HeaderStore.tsx
@@ -1,15 +1,10 @@
 import Image from "next/image"
 import { NavbarStore } from "./"
 
-
 const HeaderStore = () => {
 	return (
-		<header>
-			<Image 
-				width={200}
-				height={50}
-				src="/img/header/SHENDY-VENDY-1.png"
-				alt="Shendy Vendy"/>
+    <header>
+ 
 			<NavbarStore />
 		</header>
 	)
