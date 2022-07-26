bc300c3882060ae60930c23e5c07416e35a1415b
ljaviertovar <luisjavier_tovar@outlook.com>
2022-07-26 09:59:33 -0500

    feat: add HeroSlider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from "next/app"
 
+import "@fontsource/bebas-neue"
+import "@fontsource/poppins"
+
+import { ChakraProvider } from "@chakra-ui/react"
+import theme from "../styles/theme"
+
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<ChakraProvider resetCSS theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	)
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import type { NextPage } from "next"
 import { ShopLayout } from "../components/layouts"
-import { ChakraProvider } from "@chakra-ui/react"
+import { HeroSlider } from "../components/ui"
 
 const Home: NextPage = () => {
 	return (
-		<ChakraProvider>
-			<ShopLayout title='Shendy Vendy' pageDescription='Find the best products here.'>
-				<h1>Home Page, fores besa limpis, pero tavis lo besa más rico siempre</h1>
-			</ShopLayout>
-		</ChakraProvider>
+		<ShopLayout title='Shendy Vendy' pageDescription='Find the best products here.'>
+			<HeroSlider />
+			<span>Fuck yeah</span>
+		</ShopLayout>
 	)
 }
 
